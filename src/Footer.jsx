b166f2459503ce637e10b72533760c179e4e1417
munaimun <munaimunz@gmail.com>
2024-10-06 16:21:14 +0600

    changed the footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,37 +1,48 @@
+import { Link } from "react-router-dom";
+
+import logo from "./assets/logo.png";
+
 const Footer = () => {
   return (
-    <div className="m-auto w-full text-center bg-slate-500 mb-2">
-      {/* The Moto */}
-      <div className="italic text-3xl m-6 text-white font-semibold">
-        Accurate Appraisal & <br />
-        Brokerage Services | SM RE Inc.
-      </div>
-
-      <hr className="w-52 m-auto mb-6" />
-
-      <div className="m-auto flex justify-evenly  w-9/12 mb-2">
-        {/* Footer-Contact-Us */}
-        <div>
-          <p className="text-3xl tracking-widest font-semibold text-gray-300">
-            CONTACT <br /> US
-          </p>
-          <hr className="border-gray-400 w-12 m-auto" />
-          <p className="text-2xl mt-4">
-            Tel. <span className="underline">310-650-8798</span>
-          </p>
-          <p className="text-2xl mt-4 underline">sam@samsdod.com</p>
+    <footer className="bg-white rounded-lg shadow dark:bg-gray-400 m-4">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Link
+            to="/"
+            className="flex justify-center sm:items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <img src={logo} className="h-8" alt="Flowbite Logo" />
+          </Link>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0 dark:text-black">
+            <li>
+              <Link to="/about" className="hover:underline me-4 md:me-6">
+                APPRAISAL
+              </Link>
+            </li>
+            <li>
+              <Link to="/copy-about" className="hover:underline me-4 md:me-6">
+                PRIVATE LENDERS
+              </Link>
+            </li>
+            <li>
+              <Link to="/brokerage" className="hover:underline me-4 md:me-6">
+                BROKERAGE
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:underline">
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
-
-        {/* Footer-Visit-Us */}
-        <div className="text-3xl tracking-widest font-semibold text-gray-300">
-          Visit <br /> Us
-          <hr className="border-gray-400 w-12 m-auto" />
-          <p className="text-base font-thin text-black mt-10">
-            Hermosa Beach, CA
-          </p>
-        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-black">
+          <Link href="https://flowbite.com/" className="hover:underline"></Link>
+          All Rights Reserved.
+        </span>
       </div>
-    </div>
+    </footer>
   );
 };
 
